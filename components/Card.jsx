45cccdda787695a2ba97a8c22bc035bd04e38a3d
safin-sys <mdsafinofficial@gmail.com>
2021-09-07 23:43:17 +0600

    ' broke the build

--- components/Card.jsx
+++ components/Card.jsx
@@ -16,7 +16,7 @@ export default function Card() {
             <Box overflow="hidden">
                 <NextLink href={`/post/${slug}`}>
                     <a>
-                        <Image src={source ? source : "/placeholder.png"} width="100%" height="200px" _hover={{transform: "scale(1.2)"}} transition="transform .2s" />
+                        <Image src={source ? source : "/placeholder.png"} width="100%" height="200px" _hover={{transform: "scale(1.2)"}} transition="transform .2s" alt="Featured Article Cover" />
                     </a>
                 </NextLink>
             </Box>
@@ -24,7 +24,7 @@ export default function Card() {
                 <a>
                     <Box mt="1.25rem">
                         <Heading fontSize="1.5rem" mb=".5rem">Start here for a quick overview of everything you need to know</Heading>
-                        <Text style={textStyle}>We've crammed the most important information to help you get started with Ghost into this one post. It's your cheat-sheet to get started, and your shortcut to advanced features.</Text>
+                        <Text style={textStyle}>Weve crammed the most important information to help you get started with Ghost into this one post. Its your cheat-sheet to get started, and your shortcut to advanced features.</Text>
                     </Box>
                 </a>
             </NextLink>
