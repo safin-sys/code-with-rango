0d7cd2469f513851e8863bc9824c2a6dfee1c193
safin-sys <mdsafinofficial@gmail.com>
2021-09-08 04:01:18 +0600

    hero is resoponsive, permanent light mode

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
     return (
         <>
             {router.pathname === "/" ?
-                <Grid placeContent="center" h="50vh" bgImage="url(/cover.jpg)" bgPos="center" bgRepeat="no-repeat" bgSize="100vw 50vh">
+                <Grid placeContent="center" h={["30vh", "50vh"]} bgImage="url(/cover.jpg)" bgPos="center" bgRepeat="no-repeat" bgSize="100vw 50vh">
                     <Box color="white">
                         <Heading fontSize={{ base: "2rem", md: "3rem", lg: "3rem" }} m="0">Code with Rango</Heading>
                         <Text fontSize="1.5rem" m="0" opacity=".8">A personal tech related blog</Text>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
+import { useColorMode } from '@chakra-ui/color-mode'
 import { Container, Flex, Grid } from '@chakra-ui/layout'
+import { TabList, Tabs, Tab } from '@chakra-ui/react'
 import Head from 'next/head'
+import { useEffect } from 'react'
 import Card from '../components/Card'
 
 export default function Home() {
+	const { colorMode, toggleColorMode } = useColorMode()
+	
+	useEffect(() => {
+		colorMode === "dark" && toggleColorMode() 
+	}, [colorMode])
+
+	const tabs = ["Marshall", "Frank", "Kanye", "Kendrick"]
 	return (
 		<>
 			<Head>
@@ -10,7 +20,14 @@ export default function Home() {
 				<meta name="description" content="Learn about technology." />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<Container maxW="container.lg" centerContent>
+			<Container maxW="container.lg" centerContent mt="1rem">
+			<Tabs>
+				<TabList>
+					{/* {tabs.map((tab, i) => {
+						return <Tab key={i}>{tab}</Tab>
+					})} */}
+				</TabList>
+			</Tabs>
 				<Grid gap="1rem" templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)"]} my="1.5rem">
 					<Card />
 					<Card />
